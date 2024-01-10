112d7cacc3ab2b42fb9110625f60f7408d9ff0c0
subhankardas90 <das.subhankar90@gmail.com>
2024-01-10 20:04:38 +0530

    25-29

---
+++ src/main/java/org/example/LC7527.java
@@ -0,0 +1,27 @@
+package org.example;
+
+//LC 2095 Delete middle of a linkedlist
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+public class LC7527 {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode prev = head;
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+
+        return head;
+    }
+}
