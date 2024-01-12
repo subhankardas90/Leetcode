c94c936944983f5b6220c384ee711c23023251e5
subhankardas90 <das.subhankar90@gmail.com>
2024-01-12 14:38:42 +0530

    35-36

---
+++ src/main/java/org/example/LC7535.java
@@ -0,0 +1,26 @@
+package org.example;
+
+//LC  1372. Longest ZigZag Path in a Binary Tree
+public class LC7535 {
+    int max =0;
+    public int longestZigZag(TreeNode root) {
+        if(root ==null)
+            return -1;
+        helper(root.left, 1, false);
+        helper(root.right,1, true);
+
+        return max;
+    }
+    public void helper(TreeNode root, int step, boolean isRight){
+        if(root==null)
+            return;
+        max = Math.max(max, step);
+        if(isRight){
+            helper(root.left, step+1, false);
+            helper(root.right, 1, true);
+        }else{
+            helper(root.left, 1, false);
+            helper(root.right, step+1, true);
+        }
+    }
+}
