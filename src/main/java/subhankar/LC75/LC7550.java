f744db80e85b01946dd4a3ff1bc27b94c7ac6933
subhankardas90 <das.subhankar90@gmail.com>
2024-01-22 07:48:31 +0530

    LC 50

--- src/main/java/subhankar/LC75/LC7550.java
+++ src/main/java/subhankar/LC75/LC7550.java
@@ -1,18 +1,33 @@
 package subhankar.LC75;
+
+import java.util.Arrays;
+
 //LC 875. Koko Eating Bananas
 public class LC7550 {
-    public int minEatingSpeed(int[] piles, int H) {
-        int low = 1, high = 1000000000, k = 0;
-        while (low <= high) {
-            k = (low + high) / 2;
-            int h = 0;
-            for (int i = 0; i < piles.length; i ++)
-                h += Math.ceil(1.0 * piles[i] / k);
-            if (h > H)
-                low = k + 1;
-            else
-                high = k - 1;
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = Arrays.stream(piles).max().getAsInt();
+
+        while (low < high) {
+            int mid = low+ (high - low) / 2;
+            if (canEatAll(piles, mid, h)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
         }
+
         return low;
+    }
+
+    private boolean canEatAll(int[] piles, int speed, int h) {
+        int time = 0;
+        for (int pile : piles) {
+            time += (pile - 1) / speed + 1;
+            if (time > h) {
+                return false; // if the total time is greater than h, return false
+            }
+        }
+        return true; // if all piles can be eaten within h hours, return true
     }
 }
